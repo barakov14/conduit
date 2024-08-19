c36b0b073a6e74a743456b744e3befc8e342a933
barakov14 <playmagma4@gmail.com>
2024-08-19 15:53:35 +0500

    cdk update

--- src/app/modules/article/containers/editor/editor.component.ts
+++ src/app/modules/article/containers/editor/editor.component.ts
@@ -1,11 +1,15 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core'
+import {EditorConfig, NgxSimpleTextEditorModule, ST_BUTTONS} from 'ngx-simple-text-editor'
 
 @Component({
   selector: 'editor',
   standalone: true,
-  imports: [],
+  imports: [
+    NgxSimpleTextEditorModule
+  ],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EditorComponent {}
+export class EditorComponent {
+}
